b4bbd5ed4513481946a4c7577a2b8f3f2ec8d190
hugodose <hugo.dose@gmail.com>
2015-03-13 17:39:57 +0000

    Create fbretorno.php

---
+++ fbretorno.php
@@ -0,0 +1,15 @@
+<?php
+
+$helper = new FacebookRedirectLoginHelper();
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+  // When Facebook returns an error
+} catch(\Exception $ex) {
+  // When validation fails or other local issues
+}
+if ($session) {
+  // Logged in
+}
+
+?>
