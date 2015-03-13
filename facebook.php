e2ddcdb9b14a83d67748c941435dc08d1157fc1d
hugodose <hugo.dose@gmail.com>
2015-03-13 17:32:50 +0000

    Create facebook.php

---
+++ facebook.php
@@ -0,0 +1,19 @@
+<?php
+
+$helper = new FacebookRedirectLoginHelper('your redirect URL here');
+$loginUrl = $helper->getLoginUrl();
+// Use the login url on a link or button to redirect to Facebook for authentication
+
+$helper = new FacebookRedirectLoginHelper();
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+  // When Facebook returns an error
+} catch(\Exception $ex) {
+  // When validation fails or other local issues
+}
+if ($session) {
+  // Logged in
+}
+
+?>
